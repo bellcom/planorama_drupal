4696056b75765b9309c9db59be87161a376430ee
Dmitri Serõn <ds@bellcom.ee>
2018-03-12 15:47:47 +0200

    This is not commited changes from dasp.dk made on 2014 y.

---
+++ js/planorama.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+  jQuery('#handelsebetingelser').attr('href', 'http://dasp.dk/side/spedit-rskolens-handelsbetingelser');
+  /*
+   *jQuery( "span.template_header:contains('Pris')" ).css({display: 'none'});
+   *jQuery( "span.template_header:contains('Pris')" ).next().css({display: 'none'});
+   */
+
+  jQuery( ".deadline.price1, .deadline.price2" ).css({display: 'none'});
+});
